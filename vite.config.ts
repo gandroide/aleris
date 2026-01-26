d3568f5ee5a4fa57e7c9655865cd752755bfad26
gandroide <ajontiveros82@gmail.com>
2026-01-26 18:28:20 +0000

    preparing PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,9 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
 
-      // Archivos estáticos que se copian tal cual desde /public
       includeAssets: [
-        'favicon.svg',
+        'favicon.ico',
+        'favicon-96x96.png',
         'apple-touch-icon.png'
       ],
 
@@ -21,23 +21,22 @@ export default defineConfig({
         theme_color: '#000000',
         background_color: '#000000',
         display: 'standalone',
-        orientation: 'portrait',
         start_url: '/',
         scope: '/',
 
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
